43f8c7e20f480ea0120231b6604bcd5c79f6941c
Chris Richard <christopher.richard27@gmail.com>
2017-07-07 23:43:11 -0700

    Added variable for busybox http server port.

--- main.tf
+++ main.tf
@@ -3,15 +3,20 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
- ami = "ami-fb90bd9b" 
+ ami = "ami-fb90bd9b"
  instance_type = "t2.micro"
+
+ variable "server_port" {
+   description = "The port the server will use for HTTP requests"
+   default = 8080
+ }
 
  user_data = <<-EOF
              #!/bin/bash
              echo "Hello, World" > index.html
              nohup busybox httpd -f -p 8080 &
              EOF
- 
+
  tags {
 	 Name = "web1"
 	 tier = "web"
