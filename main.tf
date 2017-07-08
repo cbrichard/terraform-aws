00f616d17b14a0aa841af9bff0c177bf854dfd63
Chris Richard <christopher.richard27@gmail.com>
2017-07-07 23:50:18 -0700

    Moved variable declaration out of resource declaration, causing errors.

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ provider "aws" {
     region = "us-west-1"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = 8080
+}
+
 resource "aws_instance" "example" {
  ami = "ami-fb90bd9b"
  instance_type = "t2.micro"
-
- variable "server_port" {
-   description = "The port the server will use for HTTP requests"
-   default = 8080
- }
 
  user_data = <<-EOF
              #!/bin/bash
